d7ea320064ee944d086223489317f59d128325e7
roshra <roshmohanr@gmail.com>
2019-04-13 16:32:21 +0530

    readfile

--- HELLO/src/HelloWorld.java
+++ HELLO/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Just prints out
-		System.out.println("Hello World");
+		System.out.println("Hello World \n");
 
 	}
 
---
+++ HELLO/src/ReadObj.java
@@ -0,0 +1,12 @@
+
+public class ReadObj {
+
+	int x = 5;
+	
+	public static void main(String[] args) {
+		ReadObj MyObj = new ReadObj();
+		System.out.println(MyObj.x);
+
+	}
+
+}
